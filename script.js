068055660b411b46584e392810dd0b73f7f3e9ee
Arka2394 <87031423+Arka2394@users.noreply.github.com>
2023-04-14 10:19:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-const players = ["John", "Bob", "Alice", "Poppy"];
+// Given array and object
+let players = ['player1', 'player2', 'player3'];
+let person = { name: 'John', age: 30, city: 'New York' };
 
-const person = {
-  name: "John Doe",
-  age: 80,
-};
+// Create a variable team that references the players array
+let team = players;
 
-// Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+// Create a variable team1 that is a copy of the players array using spread syntax
+let team1 = [...players];
 
-window.players = players;
-window.person = person;
-window.team = team;
-window.team1 = team1;
-window.cap1 = cap1;
+// Create a variable cap1 that is a copy of the person object using object destructuring
+let cap1 = { ...person };
